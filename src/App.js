c7ee85a8ce60fcfa332f719c85e5f99fc2fce145
simon deshayes <simon.deshayes@hotmail.com>
2020-12-08 23:43:06 +0100

    svg icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+//icon
+import icon from "./ressources/icon.svg";
+
 function App() {
     return (
         <div className="App">
             <header className="App-header"></header>
+            <img src="{icon}" alt="icon"></img>
         </div>
     );
 }
